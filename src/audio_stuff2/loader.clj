42ed776c68daadb02e688169f353f44c37138ae7
Tom <tprice7@gmail.com>
2020-02-26 14:07:33 -0500

    include saved directory

--- src/audio_stuff2/loader.clj
+++ src/audio_stuff2/loader.clj
@@ -1,6 +1,6 @@
 (ns audio-stuff2.loader
   "Initial namespace loaded by the repl.
-  We avoid requiring any other namespaces in this project,
+  We avoid requiring other namespaces from this project,
   so that if an error is encountered while compiling one of those namespaces
   it won't prevent this namespace from compiling. This is handy when tweaking things.
   For this reason, functions like play and rec are interned from the control namespace
